3d45385266f9ced709def69162c8a01fdcd72ac3
root <root@UBT-TIMUR.u2b3fjikq1uu3a0jy45pbyhd4b.nx.internal.cloudapp.net>
2020-07-09 13:25:33 +0000

    ok

--- ecomb2b/settings.py
+++ ecomb2b/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 
 #ALLOWED_HOSTS = ['127.0.0.1']
 
-ALLOWED_HOSTS = ['191.238.216.168','0.0.0.0']
+ALLOWED_HOSTS = ['191.238.216.168','0.0.0.0','vendas.greenish.com.br']
 
 
 # Application definition
